81d4ec9e785448e4eb151ac863781d757ee76a93
池田奈菜子 <ikimko1103@gmail.com>
2019-10-14 23:36:21 +0900

    refactor: #5 create shops index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :shops
   get "posts/index" => "posts#index"
   get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
