833b5b69ec9e38e4404fcbc942c09c8f69ff68aa
池田奈菜子 <ikimko1103@gmail.com>
2019-11-24 21:23:57 +0900

    at moment commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/index'
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :articles
   # resources :shops
   get "shops/index" => "shops#index"
   get "shops/:id" => "shops#show"
+  get "books/index" => "books#index"
   # get "posts/index" => "posts#index"
   get "/" => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
