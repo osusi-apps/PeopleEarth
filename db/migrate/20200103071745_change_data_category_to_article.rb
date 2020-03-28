9bcf015eb2808a51017222f81d13c98e5caece17
yamiko6119 <fuchihashi.miyu@gmail.com>
2020-03-28 19:00:47 +0900

    fix: fixed migration file.

--- db/migrate/20200103071745_change_data_category_to_article.rb
+++ db/migrate/20200103071745_change_data_category_to_article.rb
@@ -1,5 +1,5 @@
 class ChangeDataCategoryToArticle < ActiveRecord::Migration[5.2]
   def change
-    change_column :articles, :category, :integer
+    change_column :articles, :category, :integer USING CAST(column_name AS integer)
   end
 end
