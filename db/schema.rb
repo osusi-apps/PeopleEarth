9e1638b28c0cf4bd26108f181d1c2287a257fdfd
yamiko6119 <fuchihashi.miyu@gmail.com>
2020-03-28 16:28:59 +0900

    fix: modified database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_040038) do
+ActiveRecord::Schema.define(version: 2020_03_28_072658) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,15 +27,15 @@ ActiveRecord::Schema.define(version: 2020_03_28_040038) do
     t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_name"
     t.string "overview"
+    t.string "image_url"
+    t.string "url"
   end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,14 +45,12 @@ ActiveRecord::Schema.define(version: 2020_03_28_040038) do
   end
 
   create_table "shops", force: :cascade do |t|
-    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "image_name"
     t.string "overview"
     t.string "url"
-    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
